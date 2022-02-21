bab9a7f60a9411073257f375bc39bb2ae3ca6470
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-02-21 18:11:37 +0100

    update

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 // https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/app_service_plan
 resource "azurerm_app_service_plan" "default" {
   name                = "func-${var.resource_long_name}"
-  location            = resource_group_location
-  resource_group_name = resource_group_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   kind                = "elastic"
   // reserved            = true // for Linux
   sku {
