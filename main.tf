26c6e8442f4f59fa3f0b3fbe4f00c274900dd442
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-02-21 23:23:14 +0100

    update to premium

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ resource "azurerm_app_service_plan" "default" {
   name                = "func-${var.resource_long_name}"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  kind                = "elastic"
+  kind                = "app"
   // reserved            = true // for Linux
   sku {
-    tier     = "ElasticPremium"
-    size     = "EP1"
+    tier = "PremiumV2"
+    size = "P1v2"
     capacity = 1
   }
 }
