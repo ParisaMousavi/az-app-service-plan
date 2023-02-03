0e6ded0e8f4c8a1417921d1d54b4dd1aca793a99
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-03 17:04:20 +0100

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_app_service_plan" "default" {
+resource "azurerm_app_service_plan" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
