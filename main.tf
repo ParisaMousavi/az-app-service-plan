c5df28a6705429f562d28261b86a4f66be409af3
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-03 17:54:13 +0100

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_app_service_plan" "this" {
+resource "azurerm_service_plan" "this" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
